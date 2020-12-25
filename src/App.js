62eedbecaf8341859a992d3171eb612a9a3a4db0
Yonarp <pranoy0211@gmail.com>
2020-12-25 19:02:38 +0530

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
+import { selectUser } from './features/counterSlice';
 import Messenger from './pages/messenger/Messenger';
 
 function App() {
+   const user = useSelector(selectUser);
   return (
     <div className="App">
-      <Messenger/>
+      {
+       user ? <Messenger/>:<h1>You need to login my man</h1>
+
+      }
     </div>
   );
 }
---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC3hzwWFiGhdifOfg0TafdB32ZzmCnXbss",
+    authDomain: "chat-app-5c1f8.firebaseapp.com",
+    projectId: "chat-app-5c1f8",
+    storageBucket: "chat-app-5c1f8.appspot.com",
+    messagingSenderId: "909419115241",
+    appId: "1:909419115241:web:27d176f32fabad021f7678",
+    measurementId: "G-ZD7TDN1B9J"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  export const db = firebaseApp.firestore();
+  export const auth = firebase.auth();
+  export const provider = firebase.auth.GoogleAuthProvider();
